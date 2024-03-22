208671c18845f9c5d79f82ee80b53a633b3323f8
Guilherme Amaral <gui.amaral123@outlook.com>
2024-03-22 21:03:34 +0000

    added: New keywords and color to game

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const blockSize = 25;
 const rows = 20;
 const cols = 20;
 let board;
-let context; 
+let context;
 
 let snakeX = blockSize * 5;
 let snakeY = blockSize * 5;
@@ -17,88 +17,97 @@ let foodY;
 
 let gameOver = false;
 
-window.onload = function() {
-    board = document.getElementById("board");
-    board.height = rows * blockSize;
-    board.width = cols * blockSize;
-    context = board.getContext("2d");
+window.onload = function () {
+  board = document.getElementById('board');
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext('2d');
 
-    placeFood();
-    document.addEventListener("keyup", changeDirection);
-    setInterval(update, 1000/10);
-}
+  placeFood();
+  document.addEventListener('keyup', changeDirection);
+  setInterval(update, 1000 / 10);
+};
 
 function update() {
-    if (gameOver) {
-        return;
+  if (gameOver) {
+    return;
+  }
+
+  context.fillStyle = '#1c1f22';
+  context.fillRect(0, 0, board.width, board.height);
+
+  context.fillStyle = '#b30000';
+  context.fillRect(foodX, foodY, blockSize, blockSize);
+
+  if (snakeX === foodX && snakeY === foodY) {
+    snakeBody.push([foodX, foodY]);
+    placeFood();
+  }
+
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  if (snakeBody.length) {
+    snakeBody[0] = [snakeX, snakeY];
+  }
+
+  snakeX += velocityX * blockSize;
+  snakeY += velocityY * blockSize;
+
+  context.fillStyle = '#26c485';
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    context.fillStyle = i % 2 === 0 ? '#26c485' : '#FFFFFF';
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+  }
+
+  // Game over conditions
+  if (
+    snakeX < 0 ||
+    snakeX >= cols * blockSize ||
+    snakeY < 0 ||
+    snakeY >= rows * blockSize
+  ) {
+    gameOver = true;
+    alert('Game Over');
+  }
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeX === snakeBody[i][0] && snakeY === snakeBody[i][1]) {
+      gameOver = true;
+      alert('Game Over');
     }
-
-    context.fillStyle="#1c1f22";
-    context.fillRect(0, 0, board.width, board.height);
-
-    context.fillStyle="#b30000";
-    context.fillRect(foodX, foodY, blockSize, blockSize);
-
-    if (snakeX === foodX && snakeY === foodY) {
-        snakeBody.push([foodX, foodY]);
-        placeFood();
-    }
-
-    for (let i = snakeBody.length-1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i-1];
-    }
-    if (snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY];
-    }
-
-    snakeX += velocityX * blockSize;
-    snakeY += velocityY * blockSize;
-
-    context.fillStyle="#26c485";
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
-    for (let i = 0; i < snakeBody.length; i++) {
-      context.fillStyle = i % 2 === 0 ?  "#26c485" : "#FFFFFF";
-      context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
-    }
-
-    // Game over conditions
-    if (snakeX < 0 || snakeX >= cols * blockSize || snakeY < 0 || snakeY >= rows * blockSize) {
-        gameOver = true;
-        alert("Game Over");
-    }
-
-    for (let i = 0; i < snakeBody.length; i++) {
-        if (snakeX === snakeBody[i][0] && snakeY === snakeBody[i][1]) {
-            gameOver = true;
-            alert("Game Over");
-        }
-    }
+  }
 }
 
 function changeDirection(e) {
-  switch(e.code) {
-    case "ArrowUp":
+  switch (e.code) {
+    case 'ArrowUp':
+    case 'KeyW':
       if (velocityY !== 1) {
         velocityX = 0;
         velocityY = -1;
       }
       break;
-    case "ArrowDown":
+    case 'ArrowDown':
+    case 'KeyS':
       if (velocityY !== -1) {
         velocityX = 0;
         velocityY = 1;
       }
       break;
-    case "ArrowLeft":
-      if (velocityX !== 1) {
-        velocityX = -1;
+    case 'ArrowRight':
+    case 'KeyD':
+      if (velocityX !== -1) {
+        velocityX = 1;
         velocityY = 0;
       }
       break;
-    case "ArrowRight":
-      if (velocityX !== -1) {
-        velocityX = 1;
+    case 'ArrowLeft':
+    case 'KeyA':
+      if (velocityX !== 1) {
+        velocityX = -1;
         velocityY = 0;
       }
       break;
@@ -106,6 +115,6 @@ function changeDirection(e) {
 }
 
 function placeFood() {
-    foodX = Math.floor(Math.random() * cols) * blockSize;
-    foodY = Math.floor(Math.random() * rows) * blockSize;
+  foodX = Math.floor(Math.random() * cols) * blockSize;
+  foodY = Math.floor(Math.random() * rows) * blockSize;
 }
